5a9ecff2531e8995c61f52dec64532b4ae22c996
yehorkhod <e.khodysko@gmail.com>
2024-10-31 14:18:22 +0200

    add git checkout script

--- homeModules/shells/bash.nix
+++ homeModules/shells/bash.nix
@@ -18,6 +18,7 @@ in
       "spp" = "pypath; venv; tmux";
       "numa" = "for a in /sys/bus/pci/devices/*; do echo 0 | sudo tee -a $a/numa_node; done";
       "cpu" = "sudo ${directory}/scripts/cpu_performance";
+      "checkout" = "${directory}/scripts/checkout";
     };
   };
 } 
