f2d684449d12646e91f1e7ac2fdc248e2a197d9b
yehorkhod <e.khodysko@gmail.com>
2025-01-02 18:44:13 +0200

    rm c-m

--- home-modules/editors/neovim/configs.lua
+++ home-modules/editors/neovim/configs.lua
@@ -7,15 +7,6 @@ vim.keymap.set('v', '<leader>y', '"+y')
 vim.keymap.set('n', '<leader>p', '"+p')
 vim.keymap.set('v', '<leader>p', '"+p')
 
--- Navigation
-vim.keymap.set('n', '<C-m>%', function() vim.cmd('vertical split') end)
-vim.keymap.set('n', '<C-m>"', function() vim.cmd('split') end)
-
-vim.keymap.set('n', '<C-m>h', '<C-W>h')
-vim.keymap.set('n', '<C-m>j', '<C-W>j')
-vim.keymap.set('n', '<C-m>k', '<C-W>k')
-vim.keymap.set('n', '<C-m>l', '<C-W>l')
-
 -- Options
 vim.opt.guicursor = ''
 vim.opt.termguicolors = true
