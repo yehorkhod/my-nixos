f126c2c4806265410938294e77986acda0f2071f
yehorkhod <e.khodysko@gmail.com>
2025-01-04 12:29:07 +0200

    fugitive

--- home-modules/editors/neovim/configs/fugitive.lua
+++ home-modules/editors/neovim/configs/fugitive.lua
@@ -1,4 +1,11 @@
 -- Keymaps
-vim.keymap.set('n', '<Space>gg', vim.cmd.Git)
-vim.keymap.set('n', '<Space>gc', ':Git commit -m ""<Left>')
-vim.keymap.set('n', '<Space>gp', ':Git push<CR>')
+vim.keymap.set('n', '<Space>gg', function() vim.cmd("Git") end)
+vim.keymap.set('n', '<Space>gc', function()
+  local commit_message = vim.fn.input("Commit message: ")
+  if commit_message ~= '' then
+    vim.cmd('Git commit -m "' .. commit_message .. '"')
+  else
+    print('Commit message cannot be empty')
+  end
+end)
+vim.keymap.set('n', '<Space>gp', function() vim.cmd("Git push") end)
