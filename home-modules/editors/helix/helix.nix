28b3053f5f16b51e369f442067731fb23e1ceff7
yehorkhod <e.khodysko@gmail.com>
2025-01-06 21:35:45 +0200

    helix

--- home-modules/editors/helix/helix.nix
+++ home-modules/editors/helix/helix.nix
@@ -1,11 +1,18 @@
-{ config, pkgs, ... }:
+{ config, pkgs, configs, ... }:
 
+let
+  theme = configs.theme;
+in
 {
   programs.helix = {
     enable = true;
 
     settings = {
-      theme = "rose_pine";
+      theme = 
+        if theme == "rose-pine" then
+          "rose_pine"
+        else
+          "everforest_dark";
       editor = {
         line-number = "relative";
       };
