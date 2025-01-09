a153cc395a326f027a31e2ca854f5d2cdec3a031
yehorkhod <e.khodysko@gmail.com>
2025-01-09 20:29:55 +0200

    colors

--- home-modules/editors/neovim/neovim.nix
+++ home-modules/editors/neovim/neovim.nix
@@ -30,23 +30,38 @@ in
     in
     [
       theme-plugin vim-sleuth luasnip cmp_luasnip friendly-snippets cmp-nvim-lsp
-      nvim-dap-ui nvim-dap-python nvim-nio nvim-web-devicons plenary-nvim nui-nvim
-      { plugin = copilot-vim;          config = toLua "vim.g.copilot_filetypes = { ['*'] = false, python = true, }"; }
-      { plugin = mason-nvim;           config = toLua "require('mason').setup()";                                    }
-      { plugin = mason-lspconfig-nvim; config = toLuaFile ./configs/mason-lspconfig.lua;                             }
-      { plugin = nvim-lspconfig;       config = toLuaFile ./configs/nvim-lspconfig.lua;                              }
-      { plugin = nvim-cmp;             config = toLuaFile ./configs/snippets.lua;                                    }
-      { plugin = nvim-dap;             config = toLuaFile ./configs/dap.lua;                                         }
-      { plugin = cloak-nvim;           config = toLuaFile ./configs/cloak.lua;                                       }
-      { plugin = undotree;             config = toLuaFile ./configs/undotree.lua;                                    }
-      { plugin = telescope-nvim;       config = toLuaFile ./configs/telescope.lua;                                   }
-      { plugin = trouble-nvim;         config = toLuaFile ./configs/trouble.lua;                                     }
-      { plugin = ChatGPT-nvim;         config = toLuaFile ./configs/chatgpt.lua;                                     }
-      { plugin = harpoon;              config = toLuaFile ./configs/harpoon.lua;                                     }
-      { plugin = oil-nvim;             config = toLuaFile ./configs/oil.lua;                                         }
-      { plugin = vim-fugitive;         config = toLuaFile ./configs/fugitive.lua;                                    }
-      { plugin = gitsigns-nvim;        config = toLuaFile ./configs/gitsigns.lua;                                    }
-      { plugin = iron-nvim;            config = toLuaFile ./configs/iron.lua;                                        }
+      luasnip nvim-dap-ui nvim-dap-python nvim-nio nvim-web-devicons plenary-nvim nui-nvim
+      { plugin = nvim-dap;             config = toLuaFile ./configs/dap.lua;             }
+      { plugin = cloak-nvim;           config = toLuaFile ./configs/cloak.lua;           }
+      { plugin = undotree;             config = toLuaFile ./configs/undotree.lua;        }
+      { plugin = telescope-nvim;       config = toLuaFile ./configs/telescope.lua;       }
+      { plugin = trouble-nvim;         config = toLuaFile ./configs/trouble.lua;         }
+      { plugin = ChatGPT-nvim;         config = toLuaFile ./configs/chatgpt.lua;         }
+      { plugin = harpoon;              config = toLuaFile ./configs/harpoon.lua;         }
+      { plugin = oil-nvim;             config = toLuaFile ./configs/oil.lua;             }
+      { plugin = vim-fugitive;         config = toLuaFile ./configs/fugitive.lua;        }
+      { plugin = gitsigns-nvim;        config = toLuaFile ./configs/gitsigns.lua;        }
+      { plugin = iron-nvim;            config = toLuaFile ./configs/iron.lua;            }
+      { plugin = mason-lspconfig-nvim; config = toLuaFile ./configs/mason-lspconfig.lua; }
+      { plugin = nvim-lspconfig;       config = toLuaFile ./configs/nvim-lspconfig.lua;  }
+      { plugin = nvim-cmp;             config = toLuaFile ./configs/snippets.lua;        }
+      {
+        plugin = mason-nvim;
+        config = toLua "require('mason').setup()";
+      }
+      {
+        plugin = copilot-vim;
+        config = toLua "vim.g.copilot_filetypes = { ['*'] = false, python = true, }";
+      }
+      {
+        plugin = nvim-colorizer-lua;
+        config = ''
+          packadd! nvim-colorizer.lua
+          lua << END
+            require('colorizer').setup()
+          END
+        '';
+      }
       {
         plugin = (nvim-treesitter.withPlugins (p: [
           p.tree-sitter-haskell
