5de3f14f717e1a0d9baf8aa1f3a987c9c1ac7556
yehorkhod <e.khodysko@gmail.com>
2025-02-01 15:34:01 +0200

    qutebrowser

--- home-modules/qutebrowser/theme.py
+++ home-modules/qutebrowser/theme.py
@@ -1,93 +1,81 @@
-    # vim:fileencoding=utf-8:foldmethod=marker
-
-palette = {
-    'fg': '#d3c6aa',
-    'red': '#e67e80',
-    'orange': '#e69875',
-    'yellow': '#dbbc7f',
-    'green': '#a7c080',
-    'aqua': '#83c092',
-    'blue': '#7fbbb3',
-    'purple': '#d699b6',
-    'grey0': '#7a8478',
-    'grey1': '#859289',
-    'grey2': '#9da9a0',
-    'statusline1': '#a7c080',
-    'statusline2': '#d3c6aa',
-    'statusline3': '#e67e80',
-    'bg_dim': '#1e2326',
-    'bg0': '#272e33',
-    'bg1': '#2e383c',
-    'bg2': '#374145',
-    'bg3': '#414b50',
-    'bg4': '#495156',
-    'bg5': '#4f5b58',
-    'bg_visual': '#4c3743',
-    'bg_red': '#493b40',
-    'bg_green': '#3c4841',
-    'bg_blue': '#384b55',
-    'bg_yellow': '#45443c',
+colors = {
+    "statusbar": {
+        "normal": {
+            "background": "#2b3339",
+            "foreground": "#d3c6aa",
+        },
+        "command": {
+            "background": "#323c41",
+            "foreground": "#d3c6aa",
+        },
+    },
+    "tabs": {
+        "even": {
+            "selected-background": "#3a454a",
+            "background": "#2b3339",
+            "foreground": "#d3c6aa",
+        },
+        "odd": {
+            "selected-background": "#3a454a",
+            "background": "#2b3339",
+            "foreground": "#d3c6aa",
+        },
+        "stop-indicator": "#e67e80",
+    },
+    "hints": {
+        "background": "#7fbbb3",
+        "foreground": "#2b3339",
+    },
+    "completion": {
+        "default": {
+            "even-background": "#2b3339",
+            "odd-background": "#323c41",
+            "foreground": "#d3c6aa",
+        },
+        "category": {
+            "background": "#3a454a",
+            "foreground": "#a7c080",
+        },
+        "selected-item": {
+            "background": "#503946",
+            "foreground": "#d3c6aa",
+        },
+    },
+    "darkmode": True
 }
 
 def setup(c):
-    c.colors.webpage.bg = palette['bg0']
+    # Statusbar
+    c.colors.statusbar.normal.bg = colors["statusbar"]["normal"]["background"]
+    c.colors.statusbar.normal.fg = colors["statusbar"]["normal"]["foreground"]
 
-    c.colors.keyhint.fg = palette['fg']
-    c.colors.keyhint.suffix.fg = palette['red']
+    c.colors.statusbar.command.bg = colors["statusbar"]["command"]["background"]
+    c.colors.statusbar.command.fg = colors["statusbar"]["command"]["foreground"]
 
-    c.colors.messages.error.bg = palette['bg_red']
-    c.colors.messages.error.fg = palette['fg']
-    c.colors.messages.info.bg = palette['bg_blue']
-    c.colors.messages.info.fg = palette['fg']
-    c.colors.messages.warning.bg = palette['bg_yellow']
-    c.colors.messages.warning.fg = palette['fg']
+    # Tabs
+    c.colors.tabs.selected.even.bg = colors["tabs"]["even"]["selected-background"]
+    c.colors.tabs.even.bg = colors["tabs"]["even"]["background"]
+    c.colors.tabs.even.fg = colors["tabs"]["even"]["foreground"]
 
-    c.colors.prompts.bg = palette['bg0']
-    c.colors.prompts.fg = palette['fg']
+    c.colors.tabs.selected.odd.bg = colors["tabs"]["odd"]["selected-background"]
+    c.colors.tabs.odd.bg = colors["tabs"]["odd"]["background"]
+    c.colors.tabs.odd.fg = colors["tabs"]["odd"]["foreground"]
 
-    c.colors.completion.category.bg = palette['bg0']
-    c.colors.completion.category.fg = palette['fg']
-    c.colors.completion.fg = palette['fg']
-    c.colors.completion.even.bg = palette['bg0']
-    c.colors.completion.odd.bg = palette['bg1']
-    c.colors.completion.match.fg = palette['red']
-    c.colors.completion.item.selected.fg = palette['fg']
-    c.colors.completion.item.selected.bg = palette['bg_yellow']
-    c.colors.completion.item.selected.border.top = palette['bg_yellow']
-    c.colors.completion.item.selected.border.bottom = palette['bg_yellow']
+    c.colors.tabs.indicator.stop = colors["tabs"]["stop-indicator"]
 
-    c.colors.completion.scrollbar.bg = palette['bg_dim']
-    c.colors.completion.scrollbar.fg = palette['fg']
+    # Hints
+    c.colors.hints.bg = colors["hints"]["background"]
+    c.colors.hints.fg =  colors["hints"]["background"]
 
-    c.colors.hints.bg = palette['bg0']
-    c.colors.hints.fg = palette['fg']
-    c.colors.hints.match.fg = palette['red']
-    c.hints.border = '0px solid black'
+    # Completion
+    c.colors.completion.even.bg = colors["completion"]["default"]["even-background"]
+    c.colors.completion.odd.bg = colors["completion"]["default"]["even-background"]
+    c.colors.completion.fg = colors["completion"]["default"]["foreground"]
 
-    c.colors.statusbar.normal.fg = palette['fg']
-    c.colors.statusbar.normal.bg = palette['bg3']
+    c.colors.completion.category.bg = colors["completion"]["category"]["background"]
+    c.colors.completion.category.fg = colors["completion"]["category"]["foreground"]
 
-    c.colors.statusbar.insert.fg = palette['bg0']
-    c.colors.statusbar.insert.bg = palette['statusline1']
+    c.colors.completion.item.selected.bg = colors["completion"]["selected-item"]["background"]
+    c.colors.completion.item.selected.fg = colors["completion"]["selected-item"]["foreground"]
 
-    c.colors.statusbar.command.fg = palette['fg']
-    c.colors.statusbar.command.bg = palette['bg0']
-
-    c.colors.statusbar.url.error.fg = palette['orange']
-    c.colors.statusbar.url.fg = palette['fg']
-    c.colors.statusbar.url.hover.fg = palette['blue']
-    c.colors.statusbar.url.success.http.fg = palette['green']
-    c.colors.statusbar.url.success.https.fg = palette['green']
-
-    c.colors.tabs.bar.bg = palette['bg_dim']
-    c.colors.tabs.even.bg = palette['bg0']
-    c.colors.tabs.odd.bg = palette['bg0']
-    c.colors.tabs.even.fg = palette['fg']
-    c.colors.tabs.odd.fg = palette['fg']
-    c.colors.tabs.selected.even.bg = palette['bg2']
-    c.colors.tabs.selected.odd.bg = palette['bg2']
-    c.colors.tabs.selected.even.fg = palette['fg']
-    c.colors.tabs.selected.odd.fg = palette['fg']
-    c.colors.tabs.indicator.start = palette['blue']
-    c.colors.tabs.indicator.stop = palette['green']
-    c.colors.tabs.indicator.error = palette['red']
+    c.colors.webpage.darkmode.enabled = True
