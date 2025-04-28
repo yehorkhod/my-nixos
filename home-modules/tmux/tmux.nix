828295f6bd4db4394ca30a9312a0a49837b878b7
yehorkhod <e.khodysko@gmail.com>
2025-04-29 02:19:21 +0300

    feat: tmux

--- home-modules/tmux/tmux.nix
+++ home-modules/tmux/tmux.nix
@@ -5,13 +5,17 @@
     enable = true;
     baseIndex = 1;
     prefix = "C-n";
+    keyMode = "vi";
     extraConfig = ''
       set -g default-terminal "tmux-256color"
       set -sg escape-time 0
-      bind h select-pane -L
-      bind j select-pane -D
-      bind k select-pane -U
-      bind l select-pane -R
+      bind-key h select-pane -L
+      bind-key j select-pane -D
+      bind-key k select-pane -U
+      bind-key l select-pane -R
+
+      bind-key -T copy-mode-vi 'v' send -X begin-selection
+      bind-key -T copy-mode-vi 'y' send -X copy-selection-and-cancel
     '';
   };
 }
