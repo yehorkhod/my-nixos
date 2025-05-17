0f7229d945ee6ac38fbf446bedb0ca4f8942cdb8
yehorkhod <e.khodysko@gmail.com>
2025-05-17 17:01:35 +0300

    feat: gc for vim✨

--- home-modules/neovim/configs/fugitive.lua
+++ home-modules/neovim/configs/fugitive.lua
@@ -2,17 +2,12 @@
 vim.keymap.set("n", "<Space>gg", function()
 	vim.cmd("Git")
 end)
-vim.keymap.set("n", "<Space>gc", function()
-	local commit_message = vim.fn.input("Commit message: ")
-	if commit_message ~= "" then
-		vim.cmd('Git commit -m "' .. commit_message .. '"')
-	else
-		print("Commit message cannot be empty!")
-	end
-end)
 vim.keymap.set("n", "<Space>gp", function()
 	vim.cmd("Git push")
 end)
 vim.keymap.set("n", "<Space>gu", function()
+	vim.cmd("Git pull")
+end)
+vim.keymap.set("n", "<Space>gc", function()
 	vim.cmd("Git pull")
 end)
