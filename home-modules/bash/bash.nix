b57d8c793fddf3777c76e5dd4167d729074e4539
yehorkhod <e.khodysko@gmail.com>
2025-07-11 01:59:40 +0300

    feat: bash aliases✨

--- home-modules/bash/bash.nix
+++ home-modules/bash/bash.nix
@@ -21,20 +21,10 @@ in {
 
       # Git
       "gs" = "git status";
-      "gi" = "git add -N";
-      "ga" = "git add --patch";
       "gp" = "git push";
-      "gur" = "git pull --rebase";
-      "gum" = "git pull --merge";
-      "gr" = "git rebase";
-      "gm" = "git merge";
       "gd" = "git diff --output-indicator-new=' ' --output-indicator-old=' '";
-      "gsw" = "git switch";
-      "gb" = "git branch";
-      "gc" = "git commit";
       "gl" =
         "git log --all --graph --pretty=format:'%C(magenta)%h %C(white) %an %ar%C(auto) %D%n%s%n'";
-      "gst" = "git stash";
     };
   };
 }
