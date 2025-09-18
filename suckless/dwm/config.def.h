ebafeec4541ba5b25928e46eb1b89b7313cfead0
yehorkhod <e.khodysko@gmail.com>
2025-09-18 23:51:33 +0300

    feat: fix colors✨

--- suckless/dwm/config.def.h
+++ suckless/dwm/config.def.h
@@ -35,7 +35,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_bg, "-nf", col_dim, "-sb", col_highlight, "-sf", col_fg, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_bg, "-nf", col_dim, "-sb", col_bg, "-sf", col_highlight, NULL };
 static const char *termcmd[]  = { "kitty", NULL };
 
 /* key definitions */
